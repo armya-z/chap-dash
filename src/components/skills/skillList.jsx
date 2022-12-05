893683773449e7c152335b61e16f63d1caaeb2c0
armya-z <111483912+armya-z@users.noreply.github.com>
2022-12-06 01:45:47 +0330

    add: redux skills

---
+++ src/components/skills/skillList.jsx
@@ -0,0 +1,26 @@
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { getSkillsFetch } from "../../features/skills/skillState";
+
+function SkillList() {
+  const skills = useSelector((state) => state.skill.skills);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getSkillsFetch());
+  }, [dispatch]);
+
+  return (
+    <div>
+      <ul>
+        {skills.map((skill) => (
+          <div key={skill.id}>
+            <li>{skill.skill}</li>
+          </div>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default SkillList;
