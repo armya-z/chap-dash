5c95e6abba756ec80c324488ad51d2776dcd0a27
armya-z <111483912+armya-z@users.noreply.github.com>
2022-12-07 01:28:46 +0330

    add: header

---
+++ src/components/Comon/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="flex justify-center bg-red-500">
+      <ul className="list-none">
+        <li>posts</li>
+        <li>projects</li>
+        <li>Skills</li>
+        <li>Team Members</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Header;
