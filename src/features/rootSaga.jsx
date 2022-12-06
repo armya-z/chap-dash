94dce0d1768152df60574cce88ad3089d00b1414
armya-z <111483912+armya-z@users.noreply.github.com>
2022-12-06 23:07:40 +0330

    update: add project saga to root

--- src/features/rootSaga.jsx
+++ src/features/rootSaga.jsx
@@ -2,11 +2,13 @@ import { all, fork } from "redux-saga/effects";
 import postSaga from "./posts/postSaga";
 import skillSaga from "./skills/skillSaga";
 import teamSaga from "./team/teamSaga";
+import projecSaga from "./Projects/projectSaga";
 
 export default function* rootSaga() {
   yield all([
     fork(postSaga),
     fork(skillSaga),
     fork(teamSaga),
+    fork(projecSaga),
   ]);
 }
