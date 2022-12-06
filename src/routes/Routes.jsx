2c86663335ca52beda038d198ea9c393af586516
armya-z <111483912+armya-z@users.noreply.github.com>
2022-12-06 23:08:35 +0330

    update: add project route

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -5,6 +5,7 @@ import {
 } from "react-router-dom";
 import PostCRUD from "../pages/postCRUD";
 import HomePage from "../pages/HomePage";
+import Projects from "../pages/Projects";
 
 const AllRoutes = () => {
   return (
@@ -12,6 +13,11 @@ const AllRoutes = () => {
       <Routes>
         <Route exact path="/posts" element={<PostCRUD />} />
         <Route exact path="/" element={<HomePage />} />
+        <Route
+          exact
+          path="/projects"
+          element={<Projects />}
+        />
       </Routes>
     </BrowserRouter>
   );
