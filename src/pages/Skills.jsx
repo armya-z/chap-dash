5c8cfcc876ad95e86810d5b93c8dcc8487643df0
armya-z <111483912+armya-z@users.noreply.github.com>
2022-12-07 22:23:59 +0330

    create: add skill page

---
+++ src/pages/Skills.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import SkillList from "../components/skills/skillList";
+
+function Skills() {
+  return (
+    <div>
+      <h1>Skills</h1>
+      <SkillList />
+    </div>
+  );
+}
+
+export default Skills;
