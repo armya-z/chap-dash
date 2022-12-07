4014ed7040bb1f37bf5c27ef54edeb66b41e33de
armya-z <111483912+armya-z@users.noreply.github.com>
2022-12-07 22:24:14 +0330

    create: add team member page

---
+++ src/pages/TeamMembers.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import TeamList from "../components/teamMember/TeamMemberList";
+
+function TeamMembers() {
+  return (
+    <div>
+      <h1>TeamMembers</h1>
+      <TeamList />
+    </div>
+  );
+}
+
+export default TeamMembers;
