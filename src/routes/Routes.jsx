2f96b5a4a6434a7a9692f2868776cd4e3d9cc7b9
armya-z <111483912+armya-z@users.noreply.github.com>
2022-12-07 22:25:06 +0330

    update: fix routes

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,25 +1,28 @@
-import {
-  Route,
-  Routes,
-  BrowserRouter,
-} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+
 import PostCRUD from "../pages/postCRUD";
 import HomePage from "../pages/HomePage";
+import TeamMembers from "../pages/TeamMembers";
 import Projects from "../pages/Projects";
+import Skills from "../pages/Skills";
 
 const AllRoutes = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route exact path="/posts" element={<PostCRUD />} />
-        <Route exact path="/" element={<HomePage />} />
-        <Route
-          exact
-          path="/projects"
-          element={<Projects />}
-        />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route exact path="/" element={<HomePage />} />{" "}
+      <Route exact path="/skills" element={<Skills />} />
+      <Route
+        exact
+        path="/team-members"
+        element={<TeamMembers />}
+      />
+      <Route exact path="/posts" element={<PostCRUD />} />
+      <Route
+        exact
+        path="/projects"
+        element={<Projects />}
+      />
+    </Routes>
   );
 };
 
